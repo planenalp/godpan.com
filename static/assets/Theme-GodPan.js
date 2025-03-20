6b60f3ca1c144206a06ec7eaa54e27973f9614ee
planenalp <111256256+planenalp@users.noreply.github.com>
2025-03-20 18:17:44 +0800

    Update Theme-GodPan.js

--- static/assets/Theme-GodPan.js
+++ static/assets/Theme-GodPan.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         /* avatar尺寸 */
-        /* 若保留圆形实心旋转 Avatar 效果就仅保留下面这段和 width + height 参数 */
+        /* 若保留圆形实心旋转 Avatar 效果就仅保留 .avatar 及 width + height 参数，其余参数和 .avatar:hover 和 .avatar:active 删除 */
         .avatar {
             width: 120px;
             height: 120px;
